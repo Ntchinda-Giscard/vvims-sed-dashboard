0345dd3383911cf62b197e7fffa2d5e76f456613
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-09-02 12:55:25 +0100

    new dashboard added

---
+++ app/components/appshell.tsx
@@ -0,0 +1,39 @@
+"use client"
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+export default function ResponsiveSizes(
+    {
+        children,
+      }: Readonly<{
+        children: React.ReactNode;
+      }>
+) {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: { base: 60, md: 70, lg: 80 } }}
+      navbar={{
+        width: { base: 100, md: 200, lg: 300 },
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Poppins } from "next/font/google";
+import '@mantine/core/styles.css';
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
+import ResponsiveSizes from "./components/appshell";
+import Provider from "./provider";
+
+const inter = Poppins({ subsets: ["latin"], weight:["500"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>
+          <ResponsiveSizes>
+            {children}
+          </ResponsiveSizes>
+        </Provider>
+      </body>
     </html>
   );
 }
