34ee3de3fe0218926d7e82d602a1656aa3295b61
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-09-23 10:26:42 +0100

    change card numbers

--- app/dashboard/components/card_info.ts
+++ app/dashboard/components/card_info.ts
@@ -7,7 +7,7 @@ import bg_g from "@/public/assets/bg_card_g.svg";
 import bg_b from "@/public/assets/bg_card_b.svg";
 
 export const card_info = [
-    {title: "Total visitor", amount: 60, perc: 12, bg_img: bg_b, img: img_p},
-    {title: "Total Vehicles", amount: 60, perc: 12, bg_img: bg_g, img: img_c},
-    {title: "Flagged Vehicles", amount: 60, perc: 12, bg_img: bg_r, img: img_cf},
+    {title: "Total visitor", amount: 0, perc: 12, bg_img: bg_b, img: img_p},
+    {title: "Total Vehicles", amount: 0, perc: 12, bg_img: bg_g, img: img_c},
+    {title: "Flagged Vehicles", amount: 0, perc: 12, bg_img: bg_r, img: img_cf},
 ]
