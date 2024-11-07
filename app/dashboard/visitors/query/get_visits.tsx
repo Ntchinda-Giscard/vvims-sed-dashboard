e2de31f4d6ea484bff94e1b0bb93503bd94914fb
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-11-07 12:12:34 +0100

    people

--- app/dashboard/visitors/query/get_visits.tsx
+++ app/dashboard/visitors/query/get_visits.tsx
@@ -2,7 +2,8 @@ import { gql } from "@apollo/client";
 
 export const GET_VISITS = gql`
 subscription GetVisits($search: String = "%691303282%", $date: timestamptz = "2100-01-01", $limit: Int! = 10, $offset: Int! = 0) {
-  visits(limit: $limit, offset: $offset, where: {
+  visits(limit: $limit, offset: $offset, 
+  where: {
     _or: [
       {visitorByVisitor: {lastname: {_ilike: $search}}},
       {visitorByVisitor: {firstname: {_ilike: $search}}}
@@ -10,7 +11,8 @@ subscription GetVisits($search: String = "%691303282%", $date: timestamptz = "21
     ],
     created_at: {_lte: $date}
   }, 
-    order_by: {created_at: desc}) {
+    order_by: {created_at: desc}) 
+    {
     check_in_at
     check_out_at
     date
@@ -68,10 +70,19 @@ subscription GetVisits($search: String = "%691303282%", $date: timestamptz = "21
 
 
 export const GET_VISITS_AGG = gql`
-subscription MyQuery($search: String = "%%", $date: date = "2100-01-01") {
-    visits_aggregate(where: {_or: [{date: {_lte: $date}}, {visitorByVisitor: {firstname: {_ilike: $search}, lastname: {_ilike: $search}, phone_number: {_ilike: $search}}}]}) {
+subscription MyQuery($search: String = "%%", $date: timestamptz = "2100-01-01") {
+    visits_aggregate(
+      where: {
+    _or: [
+      {visitorByVisitor: {lastname: {_ilike: $search}}},
+      {visitorByVisitor: {firstname: {_ilike: $search}}}
+      {visitorByVisitor: {phone_number: {_ilike: $search}}}
+    ],
+    created_at: {_lte: $date}
+  },
+      ) {
       aggregate {
         count
       }
     }
-  }`
+  }`;
